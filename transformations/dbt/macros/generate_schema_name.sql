26400f3e30b9dcce18ba7e9b9efcfdaa438c557e
AkhilBhavsar <akhilbhavsar09@gmail.com>
2026-08-03 02:15:18 +0100

    feat: support isolated Gold target overrides

--- transformations/dbt/macros/generate_schema_name.sql
+++ transformations/dbt/macros/generate_schema_name.sql
@@ -1,9 +1,19 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
 
-    {%- set governed_exact_schemas = [
-        'thesis_pac_dev_gold_internal',
+    {%- set gold_internal_schema = env_var(
+        'DBT_GOLD_INTERNAL_SCHEMA',
+        'thesis_pac_dev_gold_internal'
+    ) | trim -%}
+
+    {%- set gold_public_schema = env_var(
+        'DBT_GOLD_PUBLIC_SCHEMA',
         'thesis_pac_dev_gold_public'
+    ) | trim -%}
+
+    {%- set governed_exact_schemas = [
+        gold_internal_schema,
+        gold_public_schema
     ] -%}
 
     {%- if custom_schema_name is none -%}
